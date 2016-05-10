833c31d970464c878655dad981c3fccd77d174fa
Tjark Saul <tjark.saul@wachplan.net>
2016-05-10 10:03:40 +0200

    Initial Commit

---
+++ PrintTrayChanger/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PrintTrayChanger
+//
+//  Created by Tjark Saul on 10.5.16.
+//  Copyright © 2016 Tjark Saul. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
