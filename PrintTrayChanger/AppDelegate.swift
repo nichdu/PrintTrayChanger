a43a4f07e6a4fa90828cae057acc403fddf1dda8
Tjark Saul <tjark.saul@wachplan.net>
2016-06-01 12:31:24 +0200

    Launch at Login eingefügt

--- PrintTrayChanger/AppDelegate.swift
+++ PrintTrayChanger/AppDelegate.swift
@@ -81,6 +81,10 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSURLSessionDelegate {
             }
         }
         menu.addItem(NSMenuItem.separatorItem())
+        var loginItem = NSMenuItem(title: "Launch at Login", action: "toggleLaunchAtStartup:", keyEquivalent: "")
+        loginItem.enabled = true
+        loginItem.state = self.applicationIsInStartUpItems() ? NSOnState : NSOffState
+        menu.addItem(loginItem)
         menu.addItem(NSMenuItem(title: "Quit", action: "quit:", keyEquivalent: "Q"))
     }
     
@@ -143,7 +147,79 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSURLSessionDelegate {
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    // MARK: Launch at Login
+    func applicationIsInStartUpItems() -> Bool {
+        return (itemReferencesInLoginItems().existingReference != nil)
+    }
+    
+    func itemReferencesInLoginItems() -> (existingReference: LSSharedFileListItemRef?, lastReference: LSSharedFileListItemRef?) {
+        let itemUrl : UnsafeMutablePointer<Unmanaged<CFURL>?> = UnsafeMutablePointer<Unmanaged<CFURL>?>.alloc(1)
+        if let appUrl : NSURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath) {
+            let loginItemsRef = LSSharedFileListCreate(
+                nil,
+                kLSSharedFileListSessionLoginItems.takeRetainedValue(),
+                nil
+                ).takeRetainedValue() as LSSharedFileListRef?
+            if loginItemsRef != nil {
+                let loginItems: NSArray = LSSharedFileListCopySnapshot(loginItemsRef, nil).takeRetainedValue() as NSArray
+                print("There are \(loginItems.count) login items")
+                let lastItemRef: LSSharedFileListItemRef = loginItems.lastObject as! LSSharedFileListItemRef
+                for var i = 0; i < loginItems.count; ++i {
+                    let currentItemRef: LSSharedFileListItemRef = loginItems.objectAtIndex(i) as! LSSharedFileListItemRef
+                    if LSSharedFileListItemResolve(currentItemRef, 0, itemUrl, nil) == noErr {
+                        if let urlRef: NSURL =  itemUrl.memory?.takeRetainedValue() {
+                            print("URL Ref: \(urlRef.lastPathComponent)")
+                            if urlRef.isEqual(appUrl) {
+                                return (currentItemRef, lastItemRef)
+                            }
+                        }
+                    } else {
+                        print("Unknown login application")
+                    }
+                }
+                //The application was not found in the startup list
+                return (nil, lastItemRef)
+            }
+        }
+        return (nil, nil)
+    }
+    
+    func toggleLaunchAtStartup(sender: AnyObject) {
+        let itemReferences = itemReferencesInLoginItems()
+        let shouldBeToggled = (itemReferences.existingReference == nil)
+        let loginItemsRef = LSSharedFileListCreate(
+            nil,
+            kLSSharedFileListSessionLoginItems.takeRetainedValue(),
+            nil
+            ).takeRetainedValue() as LSSharedFileListRef?
+        if loginItemsRef != nil {
+            if shouldBeToggled {
+                if let appUrl : CFURLRef = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath) {
+                    LSSharedFileListInsertItemURL(
+                        loginItemsRef,
+                        itemReferences.lastReference,
+                        nil,
+                        nil,
+                        appUrl,
+                        nil,
+                        nil
+                    )
+                    if let item = sender as? NSMenuItem {
+                        item.state = NSOnState
+                    }
+                    print("Application was added to login items")
+                }
+            } else {
+                if let itemRef = itemReferences.existingReference {
+                    LSSharedFileListItemRemove(loginItemsRef,itemRef);
+                    if let item = sender as? NSMenuItem {
+                        item.state = NSOffState
+                    }
+                    print("Application was removed from login items")
+                }
+            }
+        }
+    }
 }
 
